48f4b2872b7f5ce069819de231b4d7489dd589be
Owen Uriel Bejarano García <owenuribg@gmail.com>
2022-12-21 20:02:56 -0600

    Agregado obtener casilleros libres por ubicacion

--- controllers/casilleros.js
+++ controllers/casilleros.js
@@ -8,7 +8,9 @@ const getCasillerosPorUbiYTam = async (req, res = response) => {
     const numCasilleros = await Casillero
         .find({ 
             ubicacion: req.body.ubicacion.toLowerCase(),
-            tamano: req.body.tamano })
+            tamano: req.body.tamano,
+            ocupado: false
+        })
         .count();
     
     res.json({
@@ -17,7 +19,36 @@ const getCasillerosPorUbiYTam = async (req, res = response) => {
     });
 }
 
+const getAllPorTam = async (req, res = response) => {
+    // Tamaños (largo x ancho x alto)
+    // S (4.3 x 16 x 25) , M (9.5 x 16 x 25) y L (20 x 16 x 25)
+    let numCasilleros = [];
+    let num;
+
+    const ubicaciones = await Casillero
+        .find()
+        .distinct('ubicacion');
+
+    for(let i=0; i<ubicaciones.length; i++){
+        num = await Casillero
+            .find({
+                ubicacion: ubicaciones[i],
+                tamano: req.body.tamano,
+                ocupado: false
+            })
+            .count();
+        numCasilleros[i] = num;
+    }
+    
+    res.json({
+        ok: true,
+        ubicaciones, 
+        numCasilleros
+    });
+}
+
 
 module.exports = {
     getCasillerosPorUbiYTam,
+    getAllPorTam
 }
--- routes/casilleros.js
+++ routes/casilleros.js
@@ -7,13 +7,15 @@ require('../controllers/casilleros');
 const express = require('express');
 const router = express.Router();
 
-const { getCasillerosPorUbiYTam } = require('../controllers/casilleros');
+const { getCasillerosPorUbiYTam, getAllPorTam} = require('../controllers/casilleros');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 router.use(validarJWT);
 
 // GET # de casilleros disponibles dada una ubicación y el tamaño
-router.get('/', getCasillerosPorUbiYTam);
+// router.get('/', getCasillerosPorUbiYTam);
 
+// GET casilleros disponibles dado un tamaño
+router.get('/', getAllPorTam);
 
 module.exports = router;
